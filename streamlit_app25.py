0b722245c13719539506a8d58c9722ca0a928874
hkhanum20columbia <58021358+hkhanum20columbia@users.noreply.github.com>
2025-11-04 03:36:25 -0800

    Add files via upload

---
+++ streamlit_app25.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import streamlit.components.v1 as components
+st.set_page_config(page_title="Hira Khanum's PMM Portfolio", page_icon=":tada:", layout= "wide")
+st.subheader("Hi, I am Hira :wave:")
+st.title("Product Marketing Manager")
+st.write("I am a data-driven, “full-stack” Product Marketer in enterprise software. And I am on a mission to move the world forward using technology to make opportunities for growth more accessible.")
+st.write("[Learn More>](https://docs.google.com/presentation/d/e/2PACX-1vS_XxmF0pVWidAxlSjff5MH4pi123YP-ugvxIS8j5nf_5gtlvlugc88O7EIDdZOJAyfyE7ZKTrqrJlE/pub?start=false&loop=false&delayms=3000)
+st.write("---")
+
+left_column, right_column=st.columns(2)
+with left_column:
+    st.header("What I do:")
+    st.write("##")
+    st.write(
+        """
+- Ensuring that market realities and only the most important attributes for customer and market adoption influence go-to-market, I AM AN AMBASSADOR OF INSIGHTS.
+- Directing the vision of the go to market plan, I AM A STRATEGIST.
+- Shaping the way the world sees our product and enabling others to tell its story, I AM A STORYTELLER.
+- Connecting insight and action to drive product adoption by shaping market perception through strategic marketing activities, I CREATE LASTING VALUE.
+"""
+        )
+
+st.write("---")
+st.header("My Projects")
+st.write("[Messaging](https://docs.google.com/document/d/e/2PACX-1vQmN2cSvEaqy_1tfDpSz-Q4a5wakVi2XIevC0q9rM-bQ5G_aT0K81VrJe-A-o4UbJFFLfSijGQvxxpD/pub)")
+st.write("[Positioning](https://docs.google.com/document/d/e/2PACX-1vTDXPV2nMiz5j8Q3tQa-B0l5TdNvu-6pGa8H3ZrVv2gGq2gvU-b0NlMrXQ6ocror3G-ggXIX7bBN5G3/pub)")
+st.write("[Pitch Decks](https://docs.google.com/presentation/d/1FwWMaVUZpsYulSinO21lWutxGHv4Efpv/edit?usp=sharing&ouid=107875029156894777800&rtpof=true&sd=true)")
+st.write("[Value Proposition Work](https://docs.google.com/spreadsheets/d/1h7OtCshZeUx6owwiY_uv78_a_-b_1TTPpATX-fDq-uE/edit?usp=sharing)")
+st.write("[Product Roadmaps](https://docs.google.com/presentation/d/e/2PACX-1vQNecZnEyzdL97Aj_WdLqI7So7y2Yd6r7HRURtccvTykHhN23RBewEaoI1cW6jKYGdgwstNixziUJbX/pub?start=false&loop=false&delayms=3000)")
+st.write("[Marketing Launch Plans](https://docs.google.com/spreadsheets/d/e/2PACX-1vTvDRGPYzJctrCtCrMQPkusyZZTWnkwwjJRPZjmPI9QVlL-_CMBEuwv2Nbnn6AYVj25uISExHGMIFAO/pubhtml)")
+st.write("[Release Plans](https://docs.google.com/presentation/d/e/2PACX-1vT1Ks-AWoryXc9yLzDwf37Pn84k4J7WCCAPiSj4ZpMcJTB-bp_gRfHQHmpBxp4pf6dv-G1geJO7Iree/pub?start=false&loop=false&delayms=3000)")
+st.write("[Product Councils & QBRs](https://docs.google.com/presentation/d/e/2PACX-1vRVuQam7miMWdPvfIE_cwmgq3-be1LGALL-zo1aqm0wOyvKrveRxhoJDkeWMFkSWJip720UNadL7anr/pub?start=false&loop=false&delayms=3000)")
+st.write("[Whitepapers & eBooks](https://drive.google.com/file/d/1eeWcIoUz7aBotNMgzHKcOV6r4ur9UUf4/view?usp=sharing)")
+st.write("[Analyst Relations](https://docs.google.com/presentation/d/11_lY_vAYScv2ZfcH1Wd2ZD2aHSvMBHLR/edit?usp=sharing&ouid=102279135462009992760&rtpof=true&sd=true)")
+st.write("[Webinars](https://docs.google.com/presentation/d/e/2PACX-1vQmqREOyu2p8MzBRoaoLhyfJXO6Gvr1Vca89lJCk1BEybqN29mL-glKBWSaJ6WGp12ozPuPB-Nw82yb/pub?start=false&loop=false&delayms=3000)")
+st.write("[Demos](https://drive.google.com/file/d/1IcjzU75IgZmhBuSKy70UORpZTjzsQ5Gn/view?usp=sharing)")
+st.write("[Communications Briefs](https://docs.google.com/document/d/e/2PACX-1vRQldN8nenLw2AD3BDQd9PinWhIQMMjPUnLVg51EinoioraJFwTulLSTdVRzudpznoPB57B5yoNeJLd/pub)")
+st.write("[Sales Enablement](https://docs.google.com/presentation/d/e/2PACX-1vThVVZt23ctuHTYpQDdvRUWUMwYuWobEaVVpqzlpXgT0v0xVh5UFcHlDq5O-UQLofvU0dF7yS7Ij-Tl/pub?start=false&loop=false&delayms=3000)")
+st.write("[Sales Trainings](https://docs.google.com/presentation/d/e/2PACX-1vRcGJlUJVPKO0nVxvRoWe9XdShEgdv0Rswh32JpjqibvvAzP6Dinfcv23Arph69sj0jTeuJOxSbM0Bo/pub?start=false&loop=false&delayms=3000)")
+st.write("[2-Pagers](https://docs.google.com/presentation/d/e/2PACX-1vRv_pxF7f9b-yXvvNuTkckM_DECqr-U1XUVxREKdUdxa2cnVzIIQA6fif_X5osbMcYG8v6_lS1IExOj/pub?start=false&loop=false&delayms=3000)")
+st.write("[1-Pagers](https://drive.google.com/file/d/1PK09u6vdaFLVLfRzGhFgMkcnnLWegjDc/view?usp=sharing)")
+st.write("[Personas & ICPs](https://docs.google.com/presentation/d/e/2PACX-1vSoE6TuNYwLFfHGlZpkcKVJrJAR0fRAQMKmoZuY9Ft2QC5a1NevMPNzqCtTuQV2Xv3ZUlop38jcObOJ/pub?start=false&loop=false&delayms=3000)")
+st.write("[Case Studies](https://docs.google.com/presentation/d/e/2PACX-1vSpPbGaJO9U-HwBDBo5FCG1p1f-oENv0_2n-4WOIVpMCyFHRb2K4Pcjz22dL0ycdRyPIKpSeGiuFoKB/pub?start=false&loop=false&delayms=3000)")
+st.write("[Cheat Sheets](https://docs.google.com/document/d/e/2PACX-1vRbIMZ__fREBPa58rkLSfowoQisspEoAzKozaKPhh2O9Wfjds-i1NBmFHSdZmbLd10_xEABfFmgxtyh/pub)")
+st.write("[Data Sheets](https://docs.google.com/document/d/e/2PACX-1vRZ-Vslhg_AKDF5iBfClFbHIvymL4vgoN0yrZPnhpKOxOOuzIAbZBjYljB45iWKeK94EScObevYMiHN/pub)")
+st.write("[Competition](https://docs.google.com/document/d/e/2PACX-1vSwh-0ipAVZE6fCRRjLRHfD9z4fIIzMtYnc6JR5lXWZ7imNS2r9oMqLYNkKcW9ZOBYo3anzTjrC2nV5/pub)")
+st.write("[Win-Loss Analysis](https://docs.google.com/presentation/d/e/2PACX-1vQ-XkotkBmc6phfZpy03Ni2FTmJJfDbuJNAppp5pT1HRrwGFjLqwo4Vmssy__OCXYwfA7BmOs9NSBrH/pub?start=false&loop=false&delayms=3000)")
+st.write("[Campaigns](https://docs.google.com/presentation/d/e/2PACX-1vSH7uzCib91vD8ZabWKv1DFJivRKzUlIOr_-pWQQRf-PZ1JCdKeoBMiTNdqPuNux6DoA1ZMhn4tTy3L/pub?start=false&loop=false&delayms=3000)")
+st.write("[Branding](https://docs.google.com/presentation/d/e/2PACX-1vQoaJ_CsZKAvDlxq7ShicowHGZWk2BIE8AgeeX2HJBkMzDNOFeUkBnX8A_arJ4HZlKeaudvztWFMQnw/pub?start=false&loop=false&delayms=3000)")
+st.write("[Open Source](https://docs.google.com/presentation/d/e/2PACX-1vQNoBUduTmM-NQplGyMp1-GD024c9SJk5fbSwUXLwjoo0zxgJgMZpAjFVxkOyj8lYqpT-GJhmdeZvW8/pub?start=false&loop=false&delayms=3000)")
+st.write("[Content Marketing](https://drive.google.com/file/d/1x59eYyBO1ebI9dr32G-GTrXyIU5tekpV/view?usp=sharing)")
+st.write("[Project Management](https://docs.google.com/spreadsheets/d/e/2PACX-1vTqlrt5DhTK8nIsqNsvQw6L2wDaj-qqqfSymq9Cn2iIG9_3lMKS0EwcmLLaBeYLS8mL8bs5TPF7yxQF/pubhtml)")
+
+         
+
+st.write("---")
+st.header(":mailbox: Get in Touch!")
+st.write("##")
+
+contact_form = """
+<form action="https://formsubmit.co/hk3033@columbia.edu" method="POST">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email"placeholder="Your email" required>
+     <textarea name="message" placeholder="Your Message here" required></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+left_column, right_column= st.columns(2)
+with left_column:
+    st.markdown(contact_form, unsafe_allow_html=True)
+with right_column:
+    st.empty()
+    
+
+
+
+
+
